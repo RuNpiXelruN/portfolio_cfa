1f76990eb5f5f5667d3a8bf6f9a2f56b504ca400
runpixelrun <justindavidson23@gmail.com>
2016-04-30 12:53:56 +0000

    Added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :contacts
   
   root 'pages#index'
   
   get '/about', to: 'pages#about'
+  
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
