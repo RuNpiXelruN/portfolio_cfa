2f06379a628964c9915b3e784c7f916f6aa7e19f
runpixelrun <justindavidson23@gmail.com>
2016-05-03 00:22:35 +0000

    Added transitions to contact section

--- app/assets/javascripts/_mainpage_transition.js
+++ app/assets/javascripts/_mainpage_transition.js
@@ -1,15 +1,49 @@
 $(document).ready(function() {
-  $('#nav-right').click(function() {
-    $(this).fadeOut();
-    $('#nav-left').fadeIn();
-    $('.home-wrapper').animate({'marginLeft':'-=100%'},500);
-    $('.about-wrapper').animate({'marginLeft':'-=100%'},500);
-  });
-  
-  $('#nav-left').click(function() {
-    $(this).fadeOut();
-    $('#nav-right').fadeIn();
-    $('.home-wrapper').animate({'marginLeft':'+=100%'},500);
-    $('.about-wrapper').animate({'marginLeft':'+=100%'},500);
-  });
+        
+
+    $('#nav-right').click(function() {
+        
+        var x = $('.contact-wrapper').css("marginTop")
+      
+        if( parseInt(x) < 0) {
+          $('#nav-up').trigger('click');
+        }
+
+        $(this).fadeOut();
+        $('#nav-left').fadeIn();
+        // $('#nav-up').trigger('click');
+        $('.home-wrapper').animate({'marginLeft':'-=100%'},500);
+        $('.about-wrapper').animate({'marginLeft':'-=100%'},500);
+    });
+    
+    
+    // ---------------------------------------
+    
+    $('#nav-left').click(function() {
+            
+        var x = $('.contact-wrapper').css("marginTop")
+        
+        if( parseInt(x) < 0 ) {
+            $('#nav-up').trigger('click');
+        }
+        
+        $(this).fadeOut();
+        $('#nav-right').fadeIn();
+        $('.home-wrapper').animate({'marginLeft':'+=100%'},500);
+        $('.about-wrapper').animate({'marginLeft':'+=100%'},500);
+    });
+    
+    // --------------------------------------- Contact arrows ------------------
+    
+    $('#nav-down').click(function() {
+        $('.contact-wrapper').animate({
+        marginTop: '-=32%'
+        }, 500); 
+    });
+    
+    $('#nav-up').click(function() {
+        $('.contact-wrapper').animate({
+        marginTop: '+=32%'
+        }, 500); 
+    });
 });
