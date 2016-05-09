090dc6b4718dd1b761aaecdae4aa1f39cfb2ead7
runpixelrun <justindavidson23@gmail.com>
2016-05-09 03:57:53 +0000

    Added 3rd page for works and jquery transitions

--- app/assets/javascripts/_work_page.js
+++ app/assets/javascripts/_work_page.js
@@ -6,6 +6,21 @@ $(document).ready(function(){
        $("#dt-clients-wrapper").animate({
            marginLeft: '0vw'
        },500);
+       $("#dt-education-wrapper").animate({
+          marginLeft: '100vw' 
+       },500);
+    });
+    
+    $("#dt-arrow-right-2").click(function(){
+       $("#dt-process-wrapper").animate({
+           marginLeft: '-200vw'
+       },500);
+       $("#dt-clients-wrapper").animate({
+           marginLeft: '-100vw'
+       },500);
+       $("#dt-education-wrapper").animate({
+          marginLeft: '0vw' 
+       },500);
     });
     
     $("#dt-arrow-left").click(function(){
@@ -15,10 +30,26 @@ $(document).ready(function(){
        $("#dt-clients-wrapper").animate({
            marginLeft: '100vw'
        },500);
+       $("#dt-education-wrapper").animate({
+          marginLeft: '200vw' 
+       },500);
+    });
+    
+    $("#dt-arrow-left-2").click(function(){
+        $("#dt-process-wrapper").animate({
+            marginLeft: '-100vw'
+        },500);
+        $("#dt-clients-wrapper").animate({
+            marginLeft: '0vw'
+        },500);
+        $("#dt-education-wrapper").animate({
+           marginLeft: '100vw' 
+        },500);
     });
     
     $(".nav-item-1").click(function(){
         $("#dt-process-wrapper").delay(600).animate({'marginLeft':'0vw'},100);
         $("#dt-clients-wrapper").delay(600).animate({'marginLeft':'100vw'},100);
+        $("#dt-education-wrapper").delay(600).animate({'marginLeft':'200vw'},100);
     });
 })
