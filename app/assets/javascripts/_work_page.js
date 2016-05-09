6f1bf835ce1d0465415bd42d5b0850af1d7c795f
runpixelrun <justindavidson23@gmail.com>
2016-05-09 01:43:41 +0000

    Added jquery for works page transitions

---
+++ app/assets/javascripts/_work_page.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#dt-arrow-right").click(function(){
+       $("#dt-process-wrapper").animate({
+           marginLeft: '-100vw'
+       },500);
+       $("#dt-clients-wrapper").animate({
+           marginLeft: '0vw'
+       });
+    });
+    
+    $("#dt-arrow-left").click(function(){
+       $("#dt-process-wrapper").animate({
+           marginLeft: '0vw'
+       },500);
+       $("#dt-clients-wrapper").animate({
+           marginLeft: '100vw'
+       });
+    });
+    
+    $(".nav-item-1").click(function(){
+        $("#dt-process-wrapper").delay(600).animate({'marginLeft':'0vw'},100);
+        $("#dt-clients-wrapper").delay(600).animate({'marginLeft':'100vw'},100);
+    });
+})
